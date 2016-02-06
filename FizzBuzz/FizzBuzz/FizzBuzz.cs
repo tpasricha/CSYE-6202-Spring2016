62bab6725cb885666d379f351efbd8400e507769
tasleen-pasricha <tasleen.25@gmail.com>
2016-02-06 14:52:51 -0500

    Committing after making final changes

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -1,14 +1,23 @@
 ﻿namespace FizzBuzz
 {
-	public class FizzBuzz
-	{
-		public string RunFizzBuzz(int number)
-		{
-			string result = number.ToString();
+    public class FizzBuzz
+    {
+        public string RunFizzBuzz(int number)
+        {
+            string result = number.ToString();
 
-			// your solution/implementation should go in here
-
-			return result;
-		}
-	}
+            if (number == 0)
+                return "0";
+            else if (number == 1)
+                return "1";
+            else if (number % 5 == 0 && number % 3 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return result;
+        }
+    }
 }
--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace FizzBuzz
+﻿using System;
+
+namespace FizzBuzz
 {
 	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
+
+    { static void Main(string[] args)
+      {
+            FizzBuzz fs = new FizzBuzz();
+           string result = fs.RunFizzBuzz(98);
+            Console.Write(result);
+            // Console.ReadKey();
+            Console.ReadLine();
+      }
 	}
 }
